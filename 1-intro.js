de54d58895be0bfdf868c5cc4ce1da95163a2f96
iamkapil <kapilrhngdle2211@gmail.com>
2023-03-06 18:20:41 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount =10
+
+if(amount <12){
+    console.log('small number');
+}
+else{
+    console.log('large number');
+}
+
---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// Modules 
+
+const names= require('./4-names')
+const sayHi= require('./5-utils')
+const data=require('./6-alternative-flavour')
+//  console.log(data)
+ require('./7-mind')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
